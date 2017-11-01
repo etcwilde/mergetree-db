990139e87b2849c39ce0067808eae9e77b541142
Evan Wilde <etcwilde@uvic.ca>
2017-11-01 10:12:12 -0700

    Adding program interface
    Can take two parameters, only requires one.
    
    First (required) parameter is the sqlite database file.
    
    Second (optional) is the HEAD commit id. Program will run faster if this
    is provided. It is required if you don't want to generate the trees for
    the entire repository.

--- src/mergetree.cpp
+++ src/mergetree.cpp
@@ -3,5 +3,24 @@
 //
 // Usage: ./mergetree <database file> <master cid>
 
+#include <iostream>
+#include <vector>
+
+#include "sqlite3.hpp"
+
 int main(int argc, char *argv[]) {
+  std::vector<std::string> args(argv, argv + argc);
+
+  if (args.size() < 2) {
+    std::cerr << "Usage: " <<  args.at(0) << " <database file> [<HEAD hash>]" << std::endl;
+    return EXIT_FAILURE;
+  }
+  SqliteDB db(args.at(1));
+
+  if (args.size() == 2) {
+    // Grab default HEAD
+    args.push_back(db.query("SELECT cid FROM commits EXCEPT SELECT parent FROM parents;").at(0).at(0));
+  }
+  std::cout << "Database: " << args.at(1) << " HEAD: " << args.at(2) << std::endl;
+
 }
